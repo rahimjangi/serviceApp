219d57fded466f0be11bd4c303ab948cea77b574
rahim jangi <rahimjangi1982@gmail.com>
2022-08-10 16:20:22 +0200

    initial commit

---
+++ customer/src/main/java/com/raiseup/customer/CustomerService.java
@@ -0,0 +1,22 @@
+package com.raiseup.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer= Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        log.info("Inside CUSTOMERSERVICE {}",customer);
+
+
+//        TODO://check if email is valid and not taken
+//        TODO://Store customer in DB
+    }
+}
